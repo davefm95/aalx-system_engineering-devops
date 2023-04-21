071d3401911c91a01dbe4fc510b88e1a1fdf6532
Femi Adetoye <davefm95@gmail.com>
2023-04-21 14:28:27 -0700

    added

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+/*
+This manifest creates a file with specified content and other info
+*/
+file { '/tmp/school':
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => "I love Puppet",
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+This file contains info about the 0x0A-configuration_management projecty
